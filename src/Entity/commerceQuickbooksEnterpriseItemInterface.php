9b7bf2f6d2cbd806939d57875f02036551a80ddb
mlevasseur <mlevasseur@acromediainc.com>
2017-01-04 14:22:09 -0800

    Working on Item entity structure.

---
+++ src/Entity/commerceQuickbooksEnterpriseItemInterface.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Drupal\commerce_quickbooks_enterprise\Entity;
+
+use Drupal\Core\Entity\ContentEntityInterface;
+
+interface commerceQuickbooksEnterpriseItemInterface extends ContentEntityInterface {
+
+}
